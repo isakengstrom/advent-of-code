e72dc297d35db8396cbaa424700eba69bebba21b
Isak Engström <isak.engstrom@gaia.se>
2022-12-01 18:01:30 +0100

    Add solutions for day 1

--- day01a/src/main.rs
+++ day01a/src/main.rs
@@ -1,3 +1,30 @@
+
 fn main() {
-    println!("Hello, day 1 a!");
+    let mut input = std::fs::read_to_string("input.txt").expect("Inputs loaded");
+    
+    // input may have a disgusting bom char in the beginning. If so, remove it  
+    if input.starts_with('\u{feff}') {
+        input.remove(0);
+    }
+    
+    // Split the items up by elf (separated by empty lines)
+    let elves = input.split("\r\n\r\n").collect::<Vec<&str>>();
+
+    // Mutable value for keeping track of the sum of calories, of the elf carrying the most calories
+    let mut highest_amount_of_calories = 0;
+    
+    for elf in elves {
+
+        let total_calories_of_elf: u32 = elf
+            .lines() // Get each item, separated by lines
+            .map(|food_item| food_item.trim().parse::<u32>().unwrap()) // parse values from string to int
+            .sum(); // sum the calories of all items of the elf
+
+        if total_calories_of_elf > highest_amount_of_calories {
+            highest_amount_of_calories = total_calories_of_elf
+        }
+    }
+    
+    println!("The elf with most calories has: {highest_amount_of_calories} cals.");
+    
 }
--- day01b/src/main.rs
+++ day01b/src/main.rs
@@ -1,3 +1,48 @@
 fn main() {
-    println!("Hello, day 1 b!");
+    let mut input = std::fs::read_to_string("input.txt").expect("Inputs loaded");
+
+    // input may have a disgusting bom char in the beginning. If so, remove it  
+    if input.starts_with('\u{feff}') {
+        input.remove(0);
+    }
+    
+    // Split the items up by elf (separated by empty lines)
+    let elves = input.split("\r\n\r\n").collect::<Vec<&str>>();
+    
+    let mut top_three: Vec<u32> = vec![0; 3];
+    let mut index_of_elf_with_fewest_cals = 0;
+    
+    for elf in elves {
+
+        let total_calories_of_elf: u32 = elf
+            .lines() // Get each item, separated by lines
+            .map(|food_item| food_item.trim().parse::<u32>().unwrap()) // parse values from string to int
+            .sum(); // sum the calories of all items of the elf
+        
+        if total_calories_of_elf > top_three[index_of_elf_with_fewest_cals] {
+            top_three[index_of_elf_with_fewest_cals] = total_calories_of_elf;
+            index_of_elf_with_fewest_cals = get_index_of_lowest(top_three.clone());
+        }
+    }
+    
+    let total: u32 = top_three.iter().sum();
+    
+    println!("The top three elves have {total} cals in total.");
+    
+}
+
+
+fn get_index_of_lowest(vals: Vec<u32>) -> usize {
+
+    let mut min = vals[0];
+    let mut min_index = 0;
+
+    for (index, &val) in vals.iter().enumerate() {
+        if val < min {
+            min = val;
+            min_index = index;
+        }
+    }
+    
+    return min_index;
 }
