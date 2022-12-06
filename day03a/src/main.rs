8c41c2ddddaa371919d6862cdd4c76d9cd2902ea
Isak Engström <isak.engstrom@gaia.se>
2022-12-06 06:31:31 +0100

    Clean up day3a

--- day03a/src/main.rs
+++ day03a/src/main.rs
@@ -1,33 +1,32 @@
-
-
 fn main() {
     //let mut input = include_str!("../input.txt").to_string();
-    let mut input = include_str!("../input.txt").to_string();
-
-    // input may have a disgusting bom char in the beginning. If so, remove it  
-    if input.starts_with('\u{feff}') {
-        input.remove(0);
-    }
+    let input = include_str!("../input.txt");
     
     let rucksacks = input
         .lines()
-        .map(|r| r.split_at(r.len() / 2)).collect::<Vec<(&str, &str)>>();
+        .map(|r| r.split_at(r.len() / 2))
+        .collect::<Vec<(&str, &str)>>();
 
+    let ascii_upper_case_location = 65;
+    let ascii_lower_case_location = 97;
     
     let mut score: u32 = 0;
     for compartments in rucksacks {
         for c in compartments.0.chars() {
-            if compartments.1.contains(c) {
-                let ascii_value = c as u32;
-                
-                if ascii_value >= 97 { 
-                    score += ascii_value - 97 + 1;
-                }
-                else {
-                    score += ascii_value - 65 + 27;
-                }
-                break;
+            if !compartments.1.contains(c) {
+                continue
             }
+
+            let ascii_value = c as u32; // Item char representation to ascii int
+
+            if ascii_value >= ascii_lower_case_location {
+                score += ascii_value - ascii_lower_case_location + 1;
+            }
+            else {
+                score += ascii_value - ascii_upper_case_location + 27;
+            }
+            
+            break;
         }
     }
     
