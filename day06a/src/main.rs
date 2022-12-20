63faafdff767e1e32d0d79fb3466b199cfbd618e
Isak Engström <isak.engstrom@gaia.se>
2022-12-20 22:41:47 +0100

    add day6a

--- day06a/src/main.rs
+++ day06a/src/main.rs
@@ -1,3 +1,20 @@
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    const MARKER_LENGTH: usize = 4;
+    
+    //let marker_start_index = include_str!("../input.example.txt")
+    let marker_start_index = include_str!("../input.txt")
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(MARKER_LENGTH) 
+        .skip(1)
+        .position(|m| HashSet::<char>::from_iter(m.iter().map(|c| *c))
+            .len() == MARKER_LENGTH
+        ) // Find first occurrence when window has 4 uniques  
+        .unwrap();
+    
+    let marker_end_index = marker_start_index + MARKER_LENGTH;
+
+    println!("{marker_end_index}");
 }
