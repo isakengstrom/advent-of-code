35b0a15121528b087f72d0cf7d2b5947a1587748
Isak Engström <isak.engstrom@gaia.se>
2023-12-05 22:02:53 +0100

    Add solution for 2023, day05, part2

--- 2023/day05/src/main.rs
+++ 2023/day05/src/main.rs
@@ -1,8 +1,10 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use std::time::Instant;
+use itertools::Itertools;
 
 fn main() {
-    //let input = include_str!("../input.example.txt");
-    let input = include_str!("../input.txt");
+    let input = include_str!("../input.example.txt");
+    //let input = include_str!("../input.txt");
 
     let mut almanac_lines = input.lines();
     let almanac_seeds: Vec<u64> = almanac_lines
@@ -55,6 +57,74 @@ fn main() {
     println!("Part 2: {ans_part_2}")
 }
 
+
+fn part_2(almanac_seeds: Vec<u64>, almanac_maps: HashMap<&str, (&str, Vec<(u64, u64, u64)>)>) -> u64 {
+    let start = Instant::now();
+
+
+    let mut min:u64 = u64::MAX;
+
+    let mut set_of_seeds: HashSet<u64> = HashSet::new();
+    let mut chunks_counter = 0;
+    for chunk in &almanac_seeds.iter().chunks(2) {
+        let seed_range_info:Vec<_>  = chunk.collect();
+        let mut seed_id = *seed_range_info[0];
+        let seed_range_length = seed_range_info[1];
+        for _ in 0 .. *seed_range_length {
+            set_of_seeds.insert(seed_id);
+            seed_id += 1;
+        }
+        chunks_counter += 1;
+
+        println!("Chunk {}, total seeds {}", chunks_counter, set_of_seeds.len());
+        let mut key = "seed";
+        let mut values:Vec<u64> = set_of_seeds.iter().cloned().collect();
+        println!(" - found {} seeds, {:?} elapsed", values.len(), start.elapsed());
+        loop {
+            if !almanac_maps.contains_key(key) {
+                break
+            }
+
+            let map = almanac_maps.get(key).unwrap();
+            println!(" - mapping from source '{}' to destination '{}', {:?} elapsed", key, map.0, start.elapsed());
+            let instructions = map.clone().1;
+
+            let mut new_values: Vec<u64> = vec![];
+            for value in values.clone() {
+                let mut continue_outer = false;
+                for instruction in &instructions {
+                    let value_min = instruction.1;
+                    let value_max = instruction.1 + instruction.2;
+                    if value < value_min || value >= value_max {
+                        continue;
+                    }
+                    let add = value - instruction.1;
+                    let new_value = instruction.0 + add;
+                    new_values.push(new_value);
+                    continue_outer = true;
+                }
+                if continue_outer {
+                    continue
+                }
+                new_values.push(value);
+            }
+
+            values = new_values;
+            key = map.0;
+        }
+        let min_of_chunk = *values.iter().min().unwrap();
+        if min_of_chunk < min {
+            min = min_of_chunk;
+        }
+        set_of_seeds = HashSet::new();
+        println!(" - chunk {} finished, current min location is {}", chunks_counter, min);
+    }
+
+
+    println!("Finished part 2 in {:?}", start.elapsed());
+    return min;
+}
+
 fn part_1(almanac_seeds: Vec<u64>, almanac_maps: HashMap<&str, (&str, Vec<(u64, u64, u64)>)>) -> u64 {
     let mut key = "seed";
 
@@ -92,8 +162,4 @@ fn part_1(almanac_seeds: Vec<u64>, almanac_maps: HashMap<&str, (&str, Vec<(u64, 
     }
 
     return *values.iter().min().unwrap();
-}
-
-fn part_2(almanac_seeds: Vec<u64>, almanac_maps: HashMap<&str, (&str, Vec<(u64, u64, u64)>)>) -> u64 {
-    return 0;
 }
